39fe5d0f14988aa9d5d2b67d1813db5cddffe20e
Young <liyang_email@163.com>
2026-05-18 22:19:00 +0800

    feat(project/css-layout): 添加 Flex 容器以及项目的属性说明数据

--- src/router/project/css-layout/-component/flex/-data.ts
+++ src/router/project/css-layout/-component/flex/-data.ts
@@ -2,22 +2,66 @@
 export const containerAttr: { name: string; desc: string }[] = [
 	{
 		name: "flex-direction",
-		desc: "主轴的方向",
+		desc: "定义主轴的方向",
 	},
 	{
 		name: "flex-wrap",
-		desc: "主轴的方向",
+		desc: "项目是否换行",
 	},
+	{
+		name: "flex-flow",
+		desc: "flex-direction 和 flex-wrap 的简写属性，默认值为 row nowrap",
+	},
+	{
+		name: "justify-content",
+		desc: "定义了项目在主轴上的对齐方式",
+	},
+	{
+		name: "align-items",
+		desc: "定义了项目在交叉轴上的对齐方式",
+	},
+	{
+		name: "align-content",
+		desc: "定义了多行时，项目在交叉轴上的对齐方式",
+	},
+	{
+		name: "gap",
+		desc: "定义了行距和列距",
+	},
+	{
+		name: "row-gap",
+		desc: "定义了行距",
+	},
+	{
+		name: "column-gap",
+		desc: "定义了列距",
+	}
 ];
 
 // flex 项目属性
 export const itemAttr: { name: string; desc: string }[] = [
 	{
-		name: "flex-direction",
-		desc: "主轴的方向",
+		name: "flex-grow",
+		desc: "定义了项目的放大比例",
 	},
 	{
-		name: "flex-wrap",
-		desc: "主轴的方向",
+		name: "flex-shrink",
+		desc: "定义了项目的缩小比例",
 	},
+	{
+		name: "flex-basis",
+		desc: "定义了项目在主轴上的初始大小",
+	},
+	{
+		name: "flex",
+		desc: "flex-grow, flex-shrink 和 flex-basis 的简写属性，默认值为 0 1 auto",
+	},
+	{
+		name: "align-self",
+		desc: "允许单个项目有与其他项目不同的对齐方式，可覆盖 align-items 属性",
+	},
+	{
+		name: "order",
+		desc: "定义了项目的排列顺序，数值越小，排列越靠前，默认为 0",
+	}
 ];
--- src/router/project/css-layout/-component/flex/index.tsx
+++ src/router/project/css-layout/-component/flex/index.tsx
@@ -1,9 +1,34 @@
+import { containerAttr, itemAttr } from "./-data";
+
 function Flex() {
-	return (
-		<div className="h-full grid grid-cols-[200px_1fr]">
-			<aside className="bg-amber-200"></aside>
-			<main></main>
-		</div>
-	);
+  return (
+    <div className="h-full grid grid-cols-[200px_1fr]">
+      <aside className="h-full min-h-0 overflow-y-auto border-r border-r-gray-200 text-xs">
+        <div>
+          <div className="text-blue-600"> Container 属性</div>
+          <div>
+            {containerAttr.map((i) => (
+              <div key={i.name}>
+                <span>{i.name}</span>
+                <span>{i.desc}</span>
+              </div>
+            ))}
+          </div>
+        </div>
+        <div>
+          <div className="text-blue-600"> Item 属性</div>
+          <div>
+            {itemAttr.map((i) => (
+              <div key={i.name}>
+                <span>{i.name}</span>
+                <span>{i.desc}</span>
+              </div>
+            ))}
+          </div>
+        </div>
+      </aside>
+      <main className="grid place-content-center">预览区域</main>
+    </div>
+  );
 }
 export default Flex;
